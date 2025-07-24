150040ccf9985c7224b8d0dbfb0bd2c933692bdd
juan <juan@prueba.com>
2025-07-24 14:32:18 +0200

    inicio

---
+++ js/juego.js
@@ -0,0 +1,23 @@
+const filas = 10;
+const columnas = 10;
+const numeroBichos = 10;
+
+function crearParcela() {
+    const parcela = document.getElementById("parcela");
+
+    for(let i = 0; i < filas * columnas; i++) {
+        const div = document.createElement("div");
+        parcela.appendChild(div);
+        div.classList.add("celda");
+    }
+}
+
+function mostrarBichos() {
+    for(let i = 0; i < numeroBichos; i++) {
+        const numeroCelda = Math.floor(Math.random() * 101);
+        
+    }
+
+}
+
+crearParcela();
